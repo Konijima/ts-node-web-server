4acbca8cc563861133abe0862d427fe7eb46df22
Konijima <mathieugermain1991@gmail.com>
2022-05-08 22:36:50 -0400

    Fixed relative path for resources

--- src/App/Routers/StaticRouter.ts
+++ src/App/Routers/StaticRouter.ts
@@ -3,7 +3,9 @@ import express, { Router } from 'express'
 
 const StaticRouter = Router()
 
+const staticPath = '../../../static/'
+
 // Serve static files such as Images & Videos from a static URL.
-StaticRouter.use('/static', express.static(resolve(__dirname, '../../static')))
+StaticRouter.use('/static', express.static(resolve(__dirname, staticPath)))
 
 export default StaticRouter
--- src/App/Routers/WebRouter.ts
+++ src/App/Routers/WebRouter.ts
@@ -3,12 +3,14 @@ import express, { Router, Request, Response } from 'express'
 
 const WebRouter = Router()
 
+const webPath = '../../../web/dist/'
+
 // Serve the angular web resources such as script, images and others.
-WebRouter.use('/', express.static(resolve(__dirname, '../../web/dist/')))
+WebRouter.use('/', express.static(resolve(__dirname, webPath)))
 
 // Any unhandled request redirect to index.html
 WebRouter.get('/*', function (request: Request, response: Response) {
-    response.sendFile(resolve(__dirname, '../../web/dist/index.html'))
+    response.sendFile(resolve(__dirname, webPath, 'index.html'))
 })
 
 export default WebRouter
