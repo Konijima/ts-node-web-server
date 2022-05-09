7e908042016c2c3bb83e2d67d8c96c46c76e8724
Konijima <mathieugermain1991@gmail.com>
2022-05-08 22:14:43 -0400

    Create default admin account automatically on the first run

--- src/Database/Database.ts
+++ src/Database/Database.ts
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose'
+import { AppLogger } from '@App/Logger'
 import { ExtendedSchemaPlugin } from '@Database/Plugins/ExtendedSchema'
 import FileStorage from '@Database/FileStorage'
-import { AppLogger } from '@App/Logger'
+import { User, UserRoles } from '@Models/User'
+
+const DEFAULT_ADMIN = {
+    username: 'admin',
+    email: `admin@${process.env.DOMAIN}`,
+    password: 'changeme',
+    role: UserRoles.ADMIN
+}
 
 class Database {
 
@@ -17,9 +25,10 @@ class Database {
     public async Connect() {
         return new Promise<void>((resolve, reject) => {
             mongoose.connect(process.env.DATABASE)
-                .then(() => {
+                .then(async () => {
                     FileStorage.Connect(mongoose.connection)
                     AppLogger.log(`Database connection established!`)
+                    await this.createDefaultAdminAccount()
                     resolve()
                 })
                 .catch((error) => {
@@ -27,6 +36,21 @@ class Database {
                     reject(error)
                 })
         })
+    }
+
+    private async createDefaultAdminAccount() {
+        let admin = await User.findOne({ role: UserRoles.ADMIN })
+        if (!admin) {
+            try {
+                const admin = new User(DEFAULT_ADMIN)
+                await admin.save()
+                AppLogger.info('Admin account created: ', DEFAULT_ADMIN)
+            }
+            catch(error) {
+                AppLogger.error(error)
+            }
+        }
+        else AppLogger.info('Admin account already exist, skipping...')
     }
 
 }
