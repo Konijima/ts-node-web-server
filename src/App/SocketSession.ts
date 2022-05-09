cec1664945fcd52469736b73e6c2f98d37ec78d4
Konijima <mathieugermain1991@gmail.com>
2022-05-08 23:29:09 -0400

    Updated SocketSession
    - Changed private to protected for extendibility
    - Removed public access to static dictionary of socket session
    - Added static method to find socket sessions
    - Added userId & sessionId properties for authentication

--- src/App/SocketSession.ts
+++ src/App/SocketSession.ts
@@ -1,15 +1,48 @@
+import { Types } from "mongoose"
 import { Socket } from "socket.io"
 import { AppLogger } from "@App/Logger"
 
 class SocketSession {
 
-    /**
-     * Access any socket session instance from this static getter
-     */
-    public static get SocketSessions() { return this.socketSessions }
-    private static socketSessions: {[socketId: string]: SocketSession} = {}
+    protected static socketSessions: {[socketId: string]: SocketSession} = {}
 
-    private socket: Socket
+    /**
+     * Get a SocketSession by socket id
+     * @returns SocketSession
+     */
+    public static getSocketSessionById(socketId: string) {
+        return this.socketSessions[socketId]
+    }
+
+    /**
+     * Get all active socket sessions from a user id
+     * @returns SocketSession[]
+     */
+    public static getSocketSessionsFromUserId(userId: Types.ObjectId) {
+        return Object.values(this.socketSessions).filter(s => s.userId === userId)
+    }
+
+    /**
+     * Get all active socket sessions from a session id
+     * @returns SocketSession[]
+     */
+    public static getSocketSessionsFromSessionId(sessionId: Types.ObjectId) {
+        return Object.values(this.socketSessions).filter(s => s.sessionId === sessionId)
+    }
+
+    /**
+     * Get the User ID, if this socket is authenticated
+     */
+    public get UserId() { return this.userId }
+    protected userId: Types.ObjectId
+
+    /**
+     * Get the Session ID, if this socket is authenticated
+     */
+    public get SessionId() { return this.sessionId }
+    protected sessionId: Types.ObjectId
+
+    protected socket: Socket
 
     constructor(socket: Socket) {
         SocketSession.socketSessions[this.socket.id] = this
@@ -26,12 +59,12 @@ class SocketSession {
     }
 
     // Handle when the connection is opened
-    private async handleConnection() {
+    protected async handleConnection() {
         
     }
 
     // Handle when the connection is closed
-    private async handleDisconnect() {
+    protected async handleDisconnect() {
         
     }
 
